c957ac025a6de5ad0ab3202a8201a065b12b2071
Mons1erDEV <111793630+Mons1erDEV@users.noreply.github.com>
2022-08-23 22:26:17 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Function to generate random number 
+function randomNumber(min, max) { 
+    return Math.floor(Math.random() * (max - min) + min);
+} 
+
+
+function getQuote(){
+    $.getJSON('https://raw.githubusercontent.com/Mons1erDEV/CSGOquotes/main/quotes.json', function(data) {
+    
+    var objLength = Object.keys(data['quotes']).length;
+    randNum = randomNumber(0,objLength);
+
+    const quoteText = document.getElementById('quote');
+    const quoteAutor = document.getElementById('autor');
+    quoteText.textContent = '';
+    quoteAutor.textContent = '';
+
+    Object.keys(data).forEach(key => {
+        var arrayQuote = data[key][randNum];
+        quoteText.textContent += arrayQuote['quote'];
+        quoteAutor.textContent += arrayQuote['autor'];
+        });
+        
+    });
+}
+
+getQuote();
